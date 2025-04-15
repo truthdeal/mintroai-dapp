aa760ec26cb66e6cfae12cd15bcc0be250cf8912
truthdeal <barisaryacantepe@gmail.com>
2025-04-15 14:26:49 +0200

    success modal width change

--- components/token-success-dialog.tsx
+++ components/token-success-dialog.tsx
@@ -32,7 +32,7 @@ export function TokenSuccessDialog({
 
   return (
     <AlertDialog open={isOpen}>
-      <AlertDialogContent className="bg-black/90 border-white/10 backdrop-blur-xl text-white p-6 max-w-md">
+      <AlertDialogContent className="bg-black/90 border-white/10 backdrop-blur-xl text-white p-6 w-[90vw] md:w-[500px]">
         <motion.div
           initial={{ scale: 0.95, opacity: 0 }}
           animate={{ scale: 1, opacity: 1 }}
